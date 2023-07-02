23f0eb14ac90190faf23212383d398631afe90b8
Kowit Wattana <nuiikowit.information@gmail.com>
2023-07-02 23:34:58 +0700

    Fist Commit

---
+++ php/src/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+    <?php
+        $host = 'db';
+        $user = 'MYSQL_USER';
+        $pass = 'MYSQL_PASSWORD';
+        $db = 'MYSQL_DATABASE';
+
+        $conn = new mysqli($host, $user, $pass, $db);
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        } else {
+            echo "Connected to MySQL server successfully!";
+        }
+
+      
+    ?>
+</body>
+</html>
